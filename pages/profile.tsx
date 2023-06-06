695a3fef365fa80a51933a7efaf7fd2ca919af1e
Eljowe <98017040+Eljowe@users.noreply.github.com>
2023-06-06 19:19:41 +0300

    aws update test

---
+++ pages/profile.tsx
@@ -0,0 +1,9 @@
+import styles from '../styles/Profile.module.css'
+
+export default function Home() {
+    return(
+        <>
+            <h1 className={styles.title}>Hello</h1>
+        </>
+    )
+}
