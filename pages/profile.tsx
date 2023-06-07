fd8c0fc292cfa45f49b4a00787efb681e0936be0
Eljowe <98017040+Eljowe@users.noreply.github.com>
2023-06-07 20:51:35 +0300

    testing fonts

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,9 +1,10 @@
 import styles from '../styles/Profile.module.css'
 
+
 export default function Home() {
     return(
-        <>
+        <div>
             <h1 className={styles.title}>Hello</h1>
-        </>
+        </div>
     )
 }
