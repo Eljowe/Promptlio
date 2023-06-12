4d70924ffe8fe2abf31a5fcee4e43f48e60b13c6
Joel <xxstokexfanxx@gmail.com>
2023-06-12 18:40:34 +0300

    custom login UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Montserrat } from 'next/font/google';
 import { Amplify, Auth } from 'aws-amplify';
+import { AmplifyProvider, Authenticator } from "@aws-amplify/ui-react";
 
 import awsconfig from '../src/aws-exports';
 
@@ -12,13 +13,15 @@ const montserrat = Montserrat({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <style jsx global>{`
-        html {
-          font-family: ${montserrat.style.fontFamily};
-        }
-      `}</style>
-      <Component {...pageProps} />
-    </>
+    <AmplifyProvider>
+      <Authenticator.Provider>
+        <style jsx global>{`
+          html {
+            font-family: ${montserrat.style.fontFamily};
+          }
+        `}</style>
+        <Component {...pageProps} />
+        </Authenticator.Provider>
+    </AmplifyProvider>
   );
 }
--- pages/profile.jsx
+++ pages/profile.jsx
@@ -1,18 +1,27 @@
-import styles from '../styles/Profile.module.css'
-import { Amplify, Auth } from 'aws-amplify';
-import { withAuthenticator } from '@aws-amplify/ui-react';
+import { useAuthenticator } from "@aws-amplify/ui-react";
+import { Amplify } from "aws-amplify";
+
+import { Profile } from "../src/layouts/Profile";
+import { Login } from "../src/components/Login";
 import '@aws-amplify/ui-react/styles.css';
-import Link from 'next/link'
+import Link from 'next/link';
 
+import awsExports from "../src/aws-exports";
+Amplify.configure(awsExports);
 
-function App({ signOut, user }) {
-  return (
-    <>
-      <h1>Hello {user.username}</h1>
-      <button onClick={signOut}>Sign out</button>
-      <Link className='w-1/2 mx-2 mt-10 text-center bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded' href='/'>Home</Link>
-    </>
-  );
+export default function App() {
+  const { user } = useAuthenticator();
+  const { signOut } = useAuthenticator();
+
+  if (user) {
+    return (
+      <>
+        <h1>Hello {user.username}</h1>
+        <button onClick={signOut}>Sign out</button>
+        <Link className='w-1/2 mx-2 mt-10 text-center bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded' href='/'>Home</Link>
+      </>
+    )
+  }
+
+  return <Login />;
 }
-
-export default withAuthenticator(App);
