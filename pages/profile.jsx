ede05d5342b51506efb250baf43cfb627e0e0fba
Eljowe <98017040+Eljowe@users.noreply.github.com>
2023-06-12 22:08:34 +0300

    new commit

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -1,5 +1,8 @@
 import { useAuthenticator } from "@aws-amplify/ui-react";
 import { Amplify } from "aws-amplify";
+import Router from 'next/router'
+
+
 
 import { Profile } from "../src/layouts/Profile";
 import { Login } from "../src/components/Login";
@@ -13,11 +16,16 @@ export default function App() {
   const { user } = useAuthenticator();
   const { signOut } = useAuthenticator();
 
+  const signOutHandler = () => {
+    signOut();
+    Router.push('/');
+  }
+
   if (user) {
     return (
       <div className="flex flex-col items-center justify-center min-h-screen">
         <h1 className="text-center text-3xl p-10">Hello {user.username}</h1>
-        <button className="w-40 mx-2 mt-10 text-center bg-blue-500 hover:text-blue-700 text-white py-2 px-4 rounded" onClick={signOut}>Sign out</button>
+        <button className="w-40 mx-2 mt-10 text-center bg-blue-500 hover:text-blue-700 text-white py-2 px-4 rounded" onClick={signOutHandler}>Sign out</button>
         <Link className='w-40 mx-2 mt-10 text-center hover:text-blue-700 text-white py-2 px-4 rounded' href='/'>Home</Link>
       </div>
     )
