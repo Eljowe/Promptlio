1b65afcf37b3ff195f41e6e684326274bc1b3b9c
Eljowe <98017040+Eljowe@users.noreply.github.com>
2023-06-12 13:06:32 +0300

    changed a to Link

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -2,6 +2,7 @@ import styles from '../styles/Profile.module.css'
 import { Amplify, Auth } from 'aws-amplify';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
+import Link from 'next/link'
 
 
 function App({ signOut, user }) {
@@ -9,7 +10,7 @@ function App({ signOut, user }) {
     <>
       <h1>Hello {user.username}</h1>
       <button onClick={signOut}>Sign out</button>
-      <a className='w-1/2 mx-2 mt-10 text-center bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded' href='/'>Home</a>
+      <Link className='w-1/2 mx-2 mt-10 text-center bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded' href='/'>Home</Link>
     </>
   );
 }
