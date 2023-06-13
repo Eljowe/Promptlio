b23aac72fb83f4b11b7a93981525f43c8e6ef240
Eljowe <98017040+Eljowe@users.noreply.github.com>
2023-06-13 16:12:46 +0300

    styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,16 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import { useAuthenticator } from "@aws-amplify/ui-react";
 
 export default function Home() {
+  const { user } = useAuthenticator();
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24`}
     >
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <h1 className="text-4xl font-bold">Hello from main</h1>
-        <Link className='p-2 text-white hover:text-blue-700 my-10 py-4' href="/profile">Profile</Link>
+        {user ? <Link className='p-2 text-white hover:text-blue-700 my-10 py-4' href="/profile">Profile</Link> : <Link className='p-2 text-white hover:text-blue-700 my-10 py-4' href="/profile">Login</Link>}
       </div>
     </main>
   )
