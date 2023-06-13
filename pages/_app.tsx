9cb2a3fef073485198133813112520c07a50603c
Joel <xxstokexfanxx@gmail.com>
2023-06-13 20:23:44 +0300

    trying to change font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,22 +5,28 @@ import { Montserrat } from 'next/font/google';
 import { Amplify, Auth } from 'aws-amplify';
 import { AmplifyProvider, Authenticator } from "@aws-amplify/ui-react";
  
-const montserrat = Montserrat({ subsets: ['latin'] });
+const montserrat = Montserrat({ 
+  weight: '400',
+  subsets: ['latin'] 
+});
 
 Amplify.configure({ ...config, ssr: true })
 Auth.configure({...config});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <AmplifyProvider>
-      <Authenticator.Provider>
-        <style jsx global>{`
-          html {
-            font-family: ${montserrat.style.fontFamily};
-          }
-        `}</style>
-        <Component {...pageProps} />
-        </Authenticator.Provider>
-    </AmplifyProvider>
+    <>
+      <style jsx global>{`
+            html {
+              font-family: ${montserrat.style.fontFamily};
+            }
+      `}</style>
+      <AmplifyProvider>
+        <Authenticator.Provider>
+          <Component {...pageProps} />
+          </Authenticator.Provider>
+      </AmplifyProvider>
+    </>
+    
   );
 }
--- pages/profile.jsx
+++ pages/profile.jsx
@@ -20,9 +20,9 @@ export default function App() {
     return (
       <div className='flex flex-col'>
         <div className='flex flex-row items-center justify-center'>
-          <h1 className="text-center text-md p-10">Hello {user.username}</h1>
-          <Link className='w-40 p-10 text-center hover:text-blue-700 text-whiterounded' href='/'>Home</Link>
-          <button className="w-40 text-center bg-blue-500 hover:text-blue-700 text-white m-10 p-5 rounded" onClick={signOutHandler}>Sign out</button>
+          <h1 className="text-center text-md p-2">Hello {user.username}</h1>
+          <Link className='w-40 p-2 text-center hover:text-blue-700 text-whiterounded' href='/'>Home</Link>
+          <button className="w-40 text-center hover:text-blue-700 text-white p-2 rounded" onClick={signOutHandler}>Sign out</button>
         </div>
         <div className="flex flex-row items-center justify-center flex-wrap sd:flex-wrap-reverse">
           <div className='w-full sm:w-[40%] md:w-1/3 lg:w-1/3 xl:w-1/4 p-4 m-5 flex flex-col items-center justify-center border-2 border-blue-600 rounded-xl'>
