7e81a147b982f6a8b66dc2e60c6341d941c5261c
Eljowe <98017040+Eljowe@users.noreply.github.com>
2023-06-15 13:21:49 +0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import { Image, useTheme } from "@aws-amplify/ui-react";
+import Link from 'next/link'
 export function Header() {
   const { tokens } = useTheme();
 
   return (
     <div>
+      <Link className='w-40 p-2 text-center hover:text-blue-700 text-whiterounded' href='/'>Home</Link>
       <Image
         alt="logo"
         src="./6.svg"
